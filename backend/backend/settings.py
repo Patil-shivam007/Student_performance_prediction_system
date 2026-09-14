e4e40798c86ded6fe54860363118062cb4609e88
Patil-shivam007 <patilshivamrut07@gamil.in>
2026-09-14 17:20:22 +0530

    Prepare Django settings for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,9 +22,9 @@ load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
